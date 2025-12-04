1dae8c903dc0ab662d2192c359842ec41ca8f1cf
Dimas Swift <dimaswift@gmail.com>
2025-12-04 05:08:12 +0300

    finished base8 watchface

--- base8/watchface/index.js
+++ base8/watchface/index.js
@@ -1,35 +1,70 @@
-(() => {
 
+(() => {
+    
     class Period {
-        constructor(color, seconds) {
-            this.color = color;
-            this.seconds = seconds;
+        constructor(color, epoch, duration) {
+            this.color = color
+            this.epoch = epoch
+            this.duration = duration
         }
      }
+
+    const NEW_MOON = 1763621220000
+    const WINTER_SOLSTICE = 1734762060000
+    const VERNAL_EQUINOX = 1742461440000
+    const MID_NIGHT = 1764795600000
+    const BIRTHDAY_ECLIPSE = 1736031937000
+
+    const SYNODIC_MONTH = 2551443800
+    const TROPICAL_YEAR = 31556925445
+    const SIDEREAL_DAY = 86164090
+    const DAY = 86400000
+    const YEAR = 31557600000
+
     const COLOR_FG = 0xFFFFFF
     const START_X = 14
-    const START_Y = 64
+    const START_Y = 73
     let CELL_SIZE = 40
 	const DIGIT_WIDTH = 16
     const DIGITS = 8
-
  	let timerId = null
-    
-    let test = null
+    let binary = null
     let time = null
     let digits = []
     let screenWidth = 0
-    let screenHeight = 0
-    let clock = 0
-    
+    let paused = false
     let period = 0
+    let vibrate = null
+    let maxBins = 0
+    let radix = 2
+    let mode = 0
+    let percentText = null
+
+    const MODES = 2
 
     let periods = [
-        new Period(0x42f5ec, 100),
-        new Period(0xFFFFFF, 1000)
+        new Period(0xFFFFFF ,MID_NIGHT, DAY),
+        new Period(0x8CE4FF, NEW_MOON, SYNODIC_MONTH),
+        new Period(0xFFC300, VERNAL_EQUINOX, TROPICAL_YEAR),
+        new Period(0x4DFFBE, WINTER_SOLSTICE, SIDEREAL_DAY),
+        new Period(0xAA60C8, BIRTHDAY_ECLIPSE, YEAR),
      ]
 
-    
+     let prettyPeriods = new Set([
+        512,
+        4096,
+        32768,
+        262144,
+        2097152,
+        16777216,
+        2097152,
+        2097152 * 2,
+        2097152 * 3,
+        2097152 * 4,
+        2097152 * 5,
+        2097152 * 6,
+        2097152 * 7]
+    )
 
     let flags = [
         0b1000,
@@ -54,6 +89,7 @@
     }
 
     function handleClick() {
+
         period++
         if(period >= periods.length) {
             period = 0
@@ -76,78 +112,149 @@
         digits.push(c)
         digits.push(d)
 
-    
         return index / 4
     }
 
-    function renderTime() {
-		
+    function build() {
+        maxBins = Math.pow(8, DIGITS)
         const deviceInfo = hmSetting.getDeviceInfo()
-
+        vibrate = hmSensor.createSensor(hmSensor.id.VIBRATE)
         screenWidth = deviceInfo.width
         screenHeight = deviceInfo.height
         CELL_SIZE = (screenWidth / 2) - (DIGIT_WIDTH) - 6
         time = hmSensor.createSensor(hmSensor.id.TIME)
 
-		test =	hmUI.createWidget(hmUI.widget.TEXT, {
+        percentText = hmUI.createWidget(hmUI.widget.TEXT, {
+			x: screenWidth / 2 - 25,
+            y: 425,
+            w: 50,
+            h: 50,
+			color: 0xffffff,
+			text_size: 24,
+			align_h: hmUI.align.CENTER_H,
+			align_v: hmUI.align.CENTER_V,
+			text_style: hmUI.text_style.NONE,
+			text: "0%" 
+		})
+
+		binary = hmUI.createWidget(hmUI.widget.TEXT, {
 			x: 0,
-			y: screenHeight - 70,
+			y: 25,
 			w: screenWidth,
 			h: 50,
 			color: 0xffffff,
-			text_size: 16,
+			text_size: 11,
 			align_h: hmUI.align.CENTER_H,
 			align_v: hmUI.align.CENTER_V,
 			text_style: hmUI.text_style.NONE,
 			text: time.utc 
-		});
+		})
+
+
+        binary.addEventListener(hmUI.event.CLICK_DOWN, (info) => {
+        
+            if (radix >= 16) {
+                radix = 2
+            }
+            else {
+                radix++
+            }
+        })
 
         let y = START_Y
 
         for (let i = 0; i < DIGITS / 2; i++) {
-            addDigit(START_X, y, (CELL_SIZE), DIGIT_WIDTH)
+           
             addDigit(START_X + CELL_SIZE + DIGIT_WIDTH, y, (CELL_SIZE), DIGIT_WIDTH)
+            addDigit(START_X, y, (CELL_SIZE), DIGIT_WIDTH)
             y += (CELL_SIZE + DIGIT_WIDTH)
         }
 
-        digits.forEach(d => {
+        digits.forEach((d,i) => {
+            let index = i
             d.addEventListener(hmUI.event.CLICK_DOWN, (info) => {
-            handleClick()
-        })
-        })
+      
+                if(index < DIGITS * 2) {
+                    handleClick()
+                }
+                else {
+                    mode++
+                    if(mode >= MODES) mode = 0
+                    applyMode()
+                }
+        })})
+
+    }
+
+    function applyMode() {
+        binary.setProperty(hmUI.prop.VISIBLE, mode == 0)
+        percentText.setProperty(hmUI.prop.VISIBLE, mode == 0)
     }
 
     function setDigit(index,digit) {
         for (let i = 0; i < 4; i++) {
-            digits[index * 4 + i].setProperty('alpha', (flags[digit] & (1 << i)) !== 0 ? 0 : 255)
+            digits[index * 4 + i].setProperty(hmUI.prop.VISIBLE, (flags[digit] & (1 << i)) == 0)
+        }
+    }
+
+    function setTime(value) {
+        binary.setProperty(hmUI.prop.TEXT, value.toString(radix))
+        for (let i = DIGITS - 1; i >= 0; i--) {
+            setDigit(i, value >> i * 3 & 0x7)
+        }
+        let normalized = value / maxBins
+        percentText.setProperty(hmUI.prop.TEXT, Math.round(normalized * 100).toString() + "%")
+    }
+
+     function warn() {
+       
+        vibrate.stop()
+        vibrate.scene = 23
+        vibrate.start()
+
+    }
+
+    function notify() {
+       
+        vibrate.stop()
+        vibrate.scene = 25
+        vibrate.start()
+
+    }
+
+    function tick() {
+
+        if (paused) return
+
+        let p = periods[period]
+        let ellapsed = (time.utc - p.epoch) % p.duration
+        let mapped = Math.floor((ellapsed * maxBins) / p.duration)
+        setTime(mapped)
+        if (prettyPeriods.has(mapped - 8)) {
+            warn()
+        }
+        else if (prettyPeriods.has(mapped)) {
+            notify()
         }
     }
 
     function entrypoint() {
-        renderTime();
-		timerId = timer.createTimer(0, 5, () => {
-            clock++
-            test.setProperty(
-                hmUI.prop.TEXT,
-                (time.utc - 694566337000).toString(8), 
-            )
-            let fraction = periods[period].seconds
-            for (let i = 0; i < DIGITS; i++) {
-                setDigit(i, (time.utc) >> i * 3 & 0x7)
-            }
-        });
+        build()
+        tick()
+		timerId = timer.createTimer(0, 64, () => {
+            tick()
+        })
     }
 
-    var __$$app$$__ = __$$hmAppManager$$__.currentApp;
-    var __$$module$$__ = __$$app$$__.current;
+    var __$$app$$__ = __$$hmAppManager$$__.currentApp
+    var __$$module$$__ = __$$app$$__.current
     __$$module$$__.module = DeviceRuntimeCore.WatchFace({
         onInit() {
-            entrypoint();
+            entrypoint()
         },
 		onDestroy() {
-            if (timerId) {
-                timer.stopTimer(timerId);
-            }
+            timerId && timer.stopTimer(timerId)
+            vibrate && vibrate.stop()
         }
-    });
-})();
+    })
+})()
